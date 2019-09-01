d0dc186c6155b56831b91185ee75b9519144be69
Lilit Otaryan <lilit_otaryan@edu.aua.am>
2019-09-01 22:45:31 +0400

    [BUG FIX] login, signin, logout

--- authentication/urls.py
+++ authentication/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.user_login, name="login"),
     path('signup/', views.signup, name="signup"),
-    path('logout/', views.logout, name="logout"),
+    path('logout/', views.user_logout, name="logout"),
+    path('<str:username>/', views.details, name="details"),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,29 +1,40 @@
 from django.http import Http404
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, logout, login
+from django.contrib.auth import authenticate, logout, login, get_user
 from . import forms
 
+
 def user_login(request):
-    username = request.POST.get('username')
-    password = request.POST.get('password')
-    if not (username and password):
-        raise Http404("Invalid credentials")
-    user = authenticate(request, username=username, password=request.password)
-    login(request, user)
-    return render(request, "./authenticate/login.html")
+    if request.user.is_authenticated:
+        return redirect('/authentication/'+str(request.user.username)+'/')
+    if request.method == 'POST':
+        form = forms.user_login(request.POST)
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        if not (username and password):
+            raise Http404("Invalid credentials")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+        else:
+            raise Http404("Username or password is not correct!")
+        return redirect('/authentication/'+str(username)+'/')
+    else:
+        form = forms.user_login()
+    return render(request, "./authentication/login.html", {"form": form})
 
 
 def user_logout(request):
     logout(request)
-    return render(request, "./authenticate/logout.html")
+    return redirect('/authentication/')
 
 
 def signup(request):
-    print(request.method)
+    if request.user.is_authenticated:
+        return redirect('/authentication/'+str(request.user.username)+'/')
     if request.method == 'POST':
-        print(request.method)
-        form = forms.signup(request.POST)
+        form = forms.user_signup(request.POST)
         username = request.POST.get('username')
         password = request.POST.get('password')
         first_name = request.POST.get('username')
@@ -33,7 +44,12 @@ def signup(request):
         User.objects.create_user(username=username, password=password, first_name=first_name, last_name=last_name)
         return redirect('/authentication/')
     else:
-        form = forms.signup()
+        form = forms.user_signup()
 
-    return render(request, "./authenticate/signup.html", {"form":form})
+    return render(request, "./authentication/signup.html", {"form": form})
 
+
+def details(request, username):
+    if not (request.user.is_authenticated and request.user.username == username):
+        raise Http404("No user loged in")
+    return render(request, "./authentication/details.html", {"user": request.user})
